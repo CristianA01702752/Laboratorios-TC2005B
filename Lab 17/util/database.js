8f405b4329049e642451e15d2d4ee69f03e87d0e
CristianA01702752 <a01702752@itesm.mx>
2022-03-22 09:01:58 -0600

    lab completo

--- Lab 17/util/database.js
+++ Lab 17/util/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'usernames',
-    password: ''
+    host: "localhost",
+    user: "root",
+    database: "usernames",
+    password: "",
 });
 
 module.exports = pool.promise();
